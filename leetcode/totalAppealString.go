dcbbb4cfae5017eff03cd536fa77721c9b577c09
sRRRs <srrrs4510@gmail.com>
2022-11-20 12:47:21 +0900

    algorithm

---
+++ leetcode/totalAppealString.go
@@ -0,0 +1,44 @@
+package leetcode
+
+import (
+	"fmt"
+	"strings"
+)
+
+func TotalAppealString() {
+	str := "code"
+
+	res := totalAppealString(str)
+	fmt.Println("distinct arr", res)
+}
+
+func totalAppealString(str string) int {
+	res := 0
+	arrStr := strings.Split(str, "")
+
+	l := len(str)
+	for i := 0; i <= l; i++ {
+		for ii := 0; ii <= l; ii++ {
+			if i >= ii { continue }
+			uniqueArr := distinct(arrStr[i:ii])
+			res += len(uniqueArr)
+		}
+	}
+
+	return res
+}
+
+
+func distinct[T string | int](arr []T) (unique []T) {
+	list := make(map[T]bool, len(arr)-1)
+
+	for _, v := range arr {
+		if !list[v] {
+			list[v] = true
+			unique = append(unique, v)
+		}
+	}
+	fmt.Println(unique)
+	return unique
+
+}
--- main.go
+++ main.go
@@ -13,10 +13,12 @@ func main() {
 	algorithm.BinarySearch()
 	algorithm.EuclidMethod()
 	algorithm.MergeSort()
+	algorithm.Distinct()
 	// leetcode
 	leetcode.ReverseWords()
 	leetcode.SelfCrossing()
 	leetcode.AggregateComponent()
+	leetcode.TotalAppealString()
 	// staticAnalyzer
 	analyzer.StaticAnalyzer()
 }
